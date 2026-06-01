177f1cee54cbd1d47936c2de4fcba7b366356665
savioferraz22 <gestortec.vsplus@gmail.com>
2026-06-01 19:11:01 -0300

    feat: add app update notice for new CLOUD BRASIL APK and adjust service worker to ignore APK files

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig(({mode}) => {
         filename: 'sw.ts',
         registerType: 'prompt',
         injectManifest: {
-          globIgnores: ['**/version.json'],
+          globIgnores: ['**/version.json', '**/*.apk'],
         },
         includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'pwa-192x192.png', 'pwa-512x512.png'],
         manifest: {
